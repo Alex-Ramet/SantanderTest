c114fecc3df894f822aada1addf4f2721251331e
Alex-Ramet <alejandro.ramet4@gmail.com>
2026-01-18 11:56:49 +0100

    Squashed commit of the following:
    commit e8534f288168bb594e33309c3ffe561bd49a2f0f
    Author: Alex-Ramet <alejandro.ramet4@gmail.com>
    Date:   Sun Jan 18 11:55:17 2026 +0100
    
        feat(docker): config all proyect with docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200'],
+    origin: [
+      'http://localhost:4200',
+      'http://localhost:80',
+      'http://localhost',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
